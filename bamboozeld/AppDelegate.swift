bd94810d9758c0dda56a46c58115a8b0b2b92c69
Claire <cyclaire25@gmail.com>
2018-01-03 23:29:27 -0800

    fixed login and connected to database

--- bamboozeld/AppDelegate.swift
+++ bamboozeld/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        Parse.initialize(
+            with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "Bamboozeled"
+                configuration.clientKey = "asdfasdkfhkkksdaj"  // set to nil assuming you have not set clientKey
+                configuration.server = "https://dry-peak-97621.herokuapp.com/parse"
+            }))       // Override point for customization after application launch.
+        
         return true
     }
 
